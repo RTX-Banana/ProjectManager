c1d83783d998d92e83a2667fc85126404beca9a1
Davin Wong <davinlamwong@gmail.com>
2020-10-03 22:58:23 -0700

    Created months array in Date Object

--- src/Date.java
+++ src/Date.java
@@ -5,6 +5,8 @@ public class Date {
     private int day;
     private int month;
     private int year;
+    private final String[] months = { "January", "February", "March", "April", "May", "June", "July", "August",
+            "September", "October", "November", "December" };
 
     public Date() {
         day = 1;
@@ -49,34 +51,7 @@ public class Date {
     }
 
     public String getMonthString() {
-        switch (month) {
-            case 1:
-                return "January";
-            case 2:
-                return "February";
-            case 3:
-                return "March";
-            case 4:
-                return "April";
-            case 5:
-                return "May";
-            case 6:
-                return "June";
-            case 7:
-                return "July";
-            case 8:
-                return "August";
-            case 9:
-                return "September";
-            case 10:
-                return "October";
-            case 11:
-                return "November";
-            case 12:
-                return "December";
-            default:
-                return "January";
-        }
+        return months[month - 1];
     }
 
     public void printDate() {
